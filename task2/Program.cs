d11822685b90514259c8e56d1354579bf09be977
maksotik <maksotik@rambler.ru>
2022-02-27 01:06:25 +0300

    Задача номер 2 по нахождению максимального числа из трех

---
+++ task2/Program.cs
@@ -0,0 +1,32 @@
+﻿int max = 0;
+
+Console.WriteLine("Введите первое число- ");
+string inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.WriteLine("Введите второе число- ");
+string inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+Console.WriteLine("Введите третье число- ");
+string inputC = Console.ReadLine();
+int numberC = int.Parse(inputC);
+
+if( max > numberA ); 
+    else
+    {
+      max = numberA;  
+    }
+
+if( max > numberB );
+    else
+    {
+       max = numberB; 
+    }
+
+if( max > numberC );
+    else
+    {
+        max = numberC;
+    }
+Console.WriteLine("Max= {0}" ,max);
