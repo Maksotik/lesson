75cf0cea5c6e9693d6b7009f5d8a9fd1a9fa460f
maksotik <maksotik@rambler.ru>
2022-02-27 00:15:09 +0300

    Первая задача по нахождению максимального числа

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ведите число A: ");
+String inputA = Console.ReadLine();
+int numberA = int.Parse(inputA);
+
+Console.WriteLine("Ведите число B: ");
+String inputB = Console.ReadLine();
+int numberB = int.Parse(inputB);
+
+if(numberA >= numberB) 
+    Console.WriteLine( "Max= {0} ", numberA );
+
+else{
+    Console.WriteLine( "Max= {0} ",numberB );
+    }
+
