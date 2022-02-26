fa201c2faeea8364af9f8f05c326eb7a4d838775
maksotik <maksotik@rambler.ru>
2022-02-27 01:27:17 +0300

    Задача номер 3 является ли число чётным

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число- ");
+string input = Console.ReadLine();
+int number = int.Parse( input );
+
+if( number % 2 == 0 )
+Console.WriteLine("Число {0} четное=))", number);    
+    else
+    {
+        Console.WriteLine("Число {0} нечетное!", number);
+    }
